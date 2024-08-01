4ef7363f94a0e45e0ca6e03c9cf0a7dc3b2bdb97
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-08-01 09:17:45 +0100

    PrintK: Add dynamic spacing
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- src/Zephyr/kernel/printkSystem.java
+++ src/Zephyr/kernel/printkSystem.java
@@ -18,12 +18,17 @@ public class printkSystem {
     private static Date initTime;
     public static int printkInit() {
         initTime = new Date();
+        Serial.serialInit();
         return 0;
     }
     public static void printk(KERN_LEVEL level, String msg) {
         long currentTime = new Date().getTime();
 
         String time = String.format("%1$,.6f", (currentTime - initTime.getTime()) / 1000.0);
-        Serial.write("[\t" + time + "] " + msg + "\n");
+        int timeLength = time.length()-7;
+        String space = "     ";
+        space = space.substring(timeLength);
+        Serial.write("["+ space + time + "] " + msg + "\n");
+
     }
 }
